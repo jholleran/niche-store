358a088321aa1eef03897ee6e284133db9380f49
Jarlath Holleran <jarlath.holleran@gmail.com>
2014-06-28 16:51:25 +0100

    displays basic products in table

--- routes/index.js
+++ routes/index.js
@@ -12,11 +12,20 @@ router.get('/', function(req, res) {
 
 
 router.get('/products', function(req, res) {
+
+
+
+    var products = [
+    	{slug : "ptz-ip-cam-1", title : "PTZ IP Camera 1", description : "Network IP Camera with Pan, Tilt & Zoom.", largeImage : "IP-PTZ-CAM-1.jpg", genre : "ip camera" },
+    	{slug : "ptz-ip-cam-2", title : "PTZ IP Camera 2", description : "Pan, Tilt & Zoom IP Camera with dual ....", largeImage : "IP-PTZ-CAM-2.jpg", genre : "ip camera" },
+    	{slug : "ptz-ip-cam-3", title : "PTZ IP Camera 3", description : "High Quality Network IP Camera with Pan, Tilt & Zoom.", largeImage : "IP-PTZ-CAM-3.jpg", genre : "ip camera" }
+    ];
+
   res.render('products', { 
   	title: 'Online Products Store',
-  	heading: 'Top Products',
+  	heading: 'IP Cameras',
     lead: 'Product titles listed by popularity rating',
-    products: []
+    products: products
   });
 });
 
