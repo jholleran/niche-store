a5104882edd27c7c601722bc5b4d6abf4c37a7d6
Jarlath Holleran <jarlath.holleran@gmail.com>
2014-06-28 15:27:40 +0100

    Initial draft of the application

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ niche-store
 ===========
 
 An online test niche store
+
+
+install dependencies:
+  $ cd niche-store && npm install
+  
+run the app:
+  $ DEBUG=niche-store ./bin/wwww
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { 
+  	title: 'Online Product Store',
+  	heading: 'Niche Site Online Store',
+    lead: 'The leading next generation Niche Test site and recommendation engine'
+  });
+});
+
+module.exports = router;
